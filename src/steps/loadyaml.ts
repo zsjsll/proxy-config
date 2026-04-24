c9661847a1b73759754e6df8c73722d18bf5c4c0
zsjsll <124119885@qq.com>
2026-04-24 16:51:03 +0800

    init

---
+++ src/steps/loadyaml.ts
@@ -0,0 +1,5 @@
+import { readFileSync, writeFileSync } from "fs"
+import { parse } from "yaml"
+
+const inputPath = "./data.yaml"
+const outputPath = "./data.json"
--- temp.ts
+++ temp.ts
@@ -1,7 +1,11 @@
 const a = undefined
 function fixArray(args: undefined | string): string[] {
-  if (args === "") return []
-  if (typeof args === "string") return args.split(/[|, ]/)
+  if (args === "") {
+    return []
+  }
+  if (typeof args === "string") {
+    return args.split(/[|, ]/)
+  }
   return []
 }
 console.log(fixArray(a))
