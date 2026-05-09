5e475a26173bca6461aafe04f3be8f370f36c93c
zsjsll <124119885@qq.com>
2026-05-09 11:48:08 +0800

    1

--- script/cli.ts
+++ script/cli.ts
@@ -7,11 +7,11 @@ const output = "../dist"
 const DELETE_PROPERTY: string[] = JSON.parse(Bun.env.DELETE_PROPERTY!)
 const PROVIDERS_URL: string[] = JSON.parse(Bun.env.PROVIDERS_URL!)
 
-const HEALTH_CHECK_URL: string = Bun.env.HEALTH_CHECK_URL!
+const CHECK_URL: string = Bun.env.CHECK_URL!
 
 const formatStringToNumber = (val: string) => (val.includes("*") ? val.split("*").reduce((prev, curr) => (prev *= Number(curr)), 1) : Number(val))
 const PROVIDERS_INTERVAL: number = formatStringToNumber(Bun.env.PROVIDERS_INTERVAL!)
-const HEALTH_CHECK_INTERVAL: number = formatStringToNumber(Bun.env.HEALTH_CHECK_INTERVAL!)
+const CHECK_INTERVAL: number = formatStringToNumber(Bun.env.CHECK_INTERVAL!)
 
 const format = true
 
@@ -44,8 +44,8 @@ clash.add(s, (airport: Airport) => {
 clash.delete(s)
 
 // 修改部分参数
-// clash.change(clash.search(["proxy-providers.*.health-check.url", "proxy-groups.*.url"]), HEALTH_CHECK_URL)
-// clash.change(clash.search(["proxy-providers.*.interval"]), PROVIDERS_INTERVAL)
-// clash.change(clash.search(["proxy-providers.*.health-check.interval", "proxy-groups.*.interval"]), HEALTH_CHECK_INTERVAL)
+clash.change(clash.search(["proxy-groups.*.url"]), CHECK_URL)
+clash.change(clash.search(["proxy-providers.*.interval"]), PROVIDERS_INTERVAL)
+clash.change(clash.search(["proxy-groups.*.interval"]), CHECK_INTERVAL)
 
 await clash.save(format)
